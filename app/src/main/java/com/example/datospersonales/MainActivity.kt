30775edd53fd2be6ddfe8def2507b9af0b461383
Santiago Solis <san010180@gmail.com>
2020-05-05 11:00:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/datospersonales/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.datospersonales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun OnClikcBoton(v: View)
+    {
+        Toast.makeText(this, "Prueba Evento Click Nombres:${edtNombres.text} ", Toast.LENGTH_LONG).show();
+    }
+}
